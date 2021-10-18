e6d976775eb53e397ef451f3d287ed819aab9963
yeongtaek, Hong <mozodev@users.noreply.github.com>
2021-10-18 15:40:35 +0900

    커스텀 테마는 삭제하고 구조를 정리합니다.

--- README.md
+++ README.md
@@ -1,8 +1,27 @@
 # drupal-project
 
-FROM https://github.com/drupal/recommended-project
+FROM [drupal/recommended-project](https://github.com/drupal/recommended-project)
 
 ```bash
-$ php -v && composer -V
+# Check dependencies.
+$ php -v && mysql -V && sqlite3 -version && composer -V
+
+# Composer create project.
 $ composer create-project mozodev/drupal-project {{ project_code }}
+
+# Add and load settings of drupal and drush.
+$ cp config/site-dev/env.example .env
+$ nano .env
+$ set -a; source .env; set +a
+
+# Install drupal.
+$ drush -y si standard \
+install_configure_form.date_default_country=KR \
+install_configure_form.date_default_timezone=Asia/Seoul \
+install_configure_form.enable_update_status_emails=NULL
+
+# uninstall
+chmod +w web/sites/default && rm -rf drive/db.* web/sites/default/settings.* web/sites/default/files/
+
 ```
+
--- config/site-dev/settings.local.php
+++ config/site-dev/settings.local.php
@@ -1,59 +1,45 @@
 <?php
 
-// @codingStandardsIgnoreFile
-
-/**
- * @file
- * Local development override configuration feature.
- *
- * To activate this feature, copy and rename it such that its path plus
- * filename is 'sites/default/settings.local.php'. Then, go to the bottom of
- * 'sites/default/settings.php' and uncomment the commented lines that mention
- * 'settings.local.php'.
- *
- * If you are using a site name in the path, such as 'sites/example.com', copy
- * this file to 'sites/example.com/settings.local.php', and uncomment the lines
- * at the bottom of 'sites/example.com/settings.php'.
- */
-
-assert_options(ASSERT_ACTIVE, TRUE);
-\Drupal\Component\Assertion\Handle::register();
-
 /**
  * 시스템 관련 필수 설정
  */
-$databases['default']['default'] =[
-  'database' => '../tmp/db.sqlite3',
-  'prefix' => '',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\sqlite',
-  'driver' => 'sqlite',
-];
-$settings['hash_salt'] = '8-PSx9EkGd-VjrimMPbGgnx2IPYnpllK5S1yNrUoq1yUzLaG5kz0gW2NclwB1bcK551FztJMgA';
-$settings['config_sync_directory'] = '../config/sync';
-$settings['trusted_host_patterns'] = [
-  '^127\.0\.0\.1$',
-  '^localhost$',
-];
-
-/**
- * 개발, 디버그용 설정
- */
-$settings['container_yamls'][] = $app_root . '/../config/site-dev/theme-dev.services.yml';
-$config['system.logging']['error_level'] = 'verbose';
-$settings['skip_permissions_hardening'] = TRUE;
-$settings['config_exclude_modules'] = ['devel', 'stage_file_proxy'];
-$settings['DRUPAL_DEBUG'] = getenv('DRUPAL_DEBUG', 1);
-// Change kint max_depth setting.
-if (class_exists('Kint')) {
-  // Set the max_depth to prevent out-of-memory.
-  \Kint::$max_depth = 4;
+$keys = ['hash_salt', 'config_sync_directory', 'trusted_host_patterns'];
+foreach ($keys as $key) {
+  if (!empty(getenv(strtoupper($key), 0))) {
+    $settings[$key] = ($key == 'trusted_host_patterns') ? 
+      explode('|', $settings['trusted_host_patterns']) : getenv(strtoupper($key));
+  }
 }
 
 /**
- * Cache 관련 설정
+ * DB 설정
  */
-$config['system.performance']['css']['preprocess'] = FALSE;
-$config['system.performance']['js']['preprocess'] = FALSE;
-$settings['cache']['bins']['render'] = 'cache.backend.null';
-$settings['cache']['bins']['page'] = 'cache.backend.null';
-$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+$databases_keys = [ 'database', 'host', 'port', 'driver', 'username', 'password', 'prefix', 'collation' ];
+foreach ($databases_keys as $key) {
+  if (!empty(getenv('DB_' . strtoupper($key), 0))) {
+    $databases['default']['default'][$key] = $value;
+  }
+}
+unset($databases['default']['default']['namespace']);
+
+/**
+ * 디버그용 설정
+ */
+$settings['DEBUG'] = getenv('DEBUG', 0);
+if (!$settings['DEBUG']) {
+  $settings['container_yamls'][] = '../config/site-dev/theme-dev.services.yml';
+  $config['system.logging']['error_level'] = 'verbose';
+  $settings['skip_permissions_hardening'] = TRUE;
+  $settings['config_exclude_modules'] = ['devel', 'stage_file_proxy'];
+  if (class_exists('Kint')) {
+    \Kint::$max_depth = 4;
+  }
+  /**
+   * Cache 관련 설정
+   */
+  $config['system.performance']['css']['preprocess'] = FALSE;
+  $config['system.performance']['js']['preprocess'] = FALSE;
+  $settings['cache']['bins']['render'] = 'cache.backend.null';
+  $settings['cache']['bins']['page'] = 'cache.backend.null';
+  $settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
+}
